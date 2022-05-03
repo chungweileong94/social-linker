e9f8a5144e2e9c58258dc8b45c3d5fe1dbba4b15
Chung Wei Leong <leongchungwei@hotmail.com>
2022-05-03 22:59:27 +0800

    Tweak input components

--- app/components/Input/Input.tsx
+++ app/components/Input/Input.tsx
@@ -3,28 +3,40 @@ import React from 'react';
 import {mixClassName} from '~/utils';
 
 type Props = {
+  type?: React.HTMLInputTypeAttribute;
   name: string;
   label: string;
   className?: string;
+  /**
+   * https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/autocomplete
+   */
+  autoComplete?: string;
   error?: boolean;
 };
 
-const Input: React.FC<Props> = ({name, label, className, error = false}) => {
+const Input: React.FC<Props> = ({
+  type = 'text',
+  name,
+  label,
+  className,
+  autoComplete,
+  error = false,
+}) => {
   return (
-    <div className={className}>
+    <div className={mixClassName('group', className)}>
       <label
         htmlFor={name}
-        className="bold block text-sm font-medium text-gray-700"
+        className="bold lg block text-sm font-medium text-gray-700 transition-colors group-focus-within:text-primary"
       >
         {label}
       </label>
       <input
-        type="text"
+        type={type}
         name={name}
         id={name}
-        autoComplete="given-name"
+        autoComplete={autoComplete}
         className={mixClassName(
-          'mt-1 block w-full rounded-md border-2 border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm',
+          'mt-1 block w-full rounded-xl border-2 border-gray-300 shadow-sm focus:border-primary focus:ring-primary sm:text-sm',
           error && 'border-rose-500',
         )}
       />
--- app/components/TextArea/TextArea.tsx
+++ app/components/TextArea/TextArea.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 
+import {mixClassName} from '~/utils';
+
 type Props = {
   name: string;
   label: string;
   rows?: number;
+  className?: string;
 };
 
-const TextArea: React.FC<Props> = ({name, label, rows = 3}) => {
+const TextArea: React.FC<Props> = ({name, label, rows = 3, className}) => {
   return (
-    <div>
-      <label htmlFor={name} className="block text-sm font-medium text-gray-700">
+    <div className={mixClassName('group', className)}>
+      <label
+        htmlFor={name}
+        className="block text-sm font-medium text-gray-700 transition-colors group-focus-within:text-primary"
+      >
         {label}
       </label>
       <div className="mt-1">
@@ -17,7 +23,7 @@ const TextArea: React.FC<Props> = ({name, label, rows = 3}) => {
           id={name}
           name={name}
           rows={rows}
-          className="mt-1 block w-full rounded-md border border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+          className="mt-1 block w-full rounded-xl border-2 border-gray-300 shadow-sm focus:border-primary focus:ring-primary sm:text-sm"
         />
       </div>
     </div>
