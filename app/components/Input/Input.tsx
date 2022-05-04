32bba6560db313876c1848e1764f773282fa3957
Chung Wei Leong <leongchungwei@hotmail.com>
2022-05-04 23:17:56 +0800

    Implement form validation

--- app/components/Input/Input.tsx
+++ app/components/Input/Input.tsx
@@ -11,7 +11,9 @@ type Props = {
    * https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/autocomplete
    */
   autoComplete?: string;
+  required?: boolean;
   error?: boolean;
+  helperText?: string;
 };
 
 const Input: React.FC<Props> = ({
@@ -20,13 +22,18 @@ const Input: React.FC<Props> = ({
   label,
   className,
   autoComplete,
+  required = false,
   error = false,
+  helperText,
 }) => {
   return (
     <div className={mixClassName('group', className)}>
       <label
         htmlFor={name}
-        className="bold lg block text-sm font-medium text-gray-700 transition-colors group-focus-within:text-primary"
+        className={mixClassName(
+          'bold lg block text-sm font-medium text-gray-700 transition-colors group-focus-within:text-primary',
+          error && 'text-rose-500',
+        )}
       >
         {label}
       </label>
@@ -35,11 +42,22 @@ const Input: React.FC<Props> = ({
         name={name}
         id={name}
         autoComplete={autoComplete}
+        required={required}
         className={mixClassName(
           'mt-1 block w-full rounded-xl border-2 border-gray-300 shadow-sm focus:border-primary focus:ring-primary sm:text-sm',
           error && 'border-rose-500',
         )}
       />
+      {!!helperText && (
+        <span
+          className={mixClassName(
+            'text-xs',
+            error ? 'text-rose-500' : 'text-gray-400',
+          )}
+        >
+          {helperText}
+        </span>
+      )}
     </div>
   );
 };
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,9 +1,15 @@
-import {MetaFunction} from '@remix-run/node';
-import {Form} from '@remix-run/react';
+import {ActionFunction, MetaFunction, redirect} from '@remix-run/node';
+import {Form, useActionData} from '@remix-run/react';
 import Typist from 'react-text-typist';
+import {z} from 'zod';
 
+import {Button} from '~/components/Button';
 import {Input} from '~/components/Input';
 import {TextArea} from '~/components/TextArea';
+
+type FormErrors = {
+  title?: string[];
+};
 
 export const meta: MetaFunction = () => {
   return {
@@ -12,7 +18,22 @@ export const meta: MetaFunction = () => {
   };
 };
 
+export const action: ActionFunction = async ({request}) => {
+  const formData = await request.formData();
+  const values = Object.fromEntries(formData.entries());
+
+  const validateResult = z
+    .object({title: z.string().min(1, 'Title is required')})
+    .safeParse(values);
+  if (!validateResult.success) {
+    return validateResult.error.formErrors.fieldErrors as FormErrors;
+  }
+
+  return redirect(``);
+};
+
 const Index = () => {
+  const errors = useActionData<FormErrors>();
   return (
     <div className="container mx-auto flex flex-col items-center px-4 py-20">
       <h1 className="mb-5 text-3xl">
@@ -27,10 +48,22 @@ const Index = () => {
       </h1>
       <p className="mb-20 opacity-50">Create your own social bio now!</p>
 
-      <Form className="grid w-full gap-4 md:w-3/4 lg:w-2/3 xl:w-1/2">
-        <Input name="title" label="Title" className="w-full" />
+      <Form
+        method="post"
+        className="grid w-full gap-4 md:w-3/4 lg:w-2/3 xl:w-1/2"
+      >
+        <Input
+          name="title"
+          label="Title"
+          required
+          className="w-full"
+          error={!!errors?.title}
+          helperText={errors?.title?.[0]}
+        />
 
-        <TextArea name="description" label="Description" />
+        <TextArea name="description" label="Description" className="mb-10" />
+
+        <Button type="submit">Create My Social Bio</Button>
       </Form>
     </div>
   );
